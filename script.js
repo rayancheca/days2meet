92e45a536f771727ff5b159eff143359626fc88c
Rayan Karim Checa <rayanchecakarim@gmail.com>
2024-09-16 17:47:47 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const targetDate = new Date('December 20, 2024 15:00:00').getTime();
+const targetDate = new Date('December 20, 2024 12:00:00').getTime();
 
 function updateCountdown() {
     const now = new Date().getTime();
